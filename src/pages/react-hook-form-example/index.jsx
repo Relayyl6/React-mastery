4675391a635dfe835546411ad98d6aaafb67015e
Oseghale Leonard <oseghaleleonard39@gmail.com>
2025-04-05 00:05:33 -0700

    MAde vital changes to code base

--- src/pages/react-hook-form-example/index.jsx
+++ src/pages/react-hook-form-example/index.jsx
@@ -10,7 +10,7 @@ function ReactHookFormExamplePage() {
 
     const onSubmitForm = (formData) => {
         console.log(formData);
-    }
+    } 
 
     return (
         <div>
@@ -18,16 +18,48 @@ function ReactHookFormExamplePage() {
             <form onSubmit={handleSubmit(onSubmitForm)}>
                 <div>
                     <label>Email</label>
-                    <input {...register('email')}
+                    <input {...register('email', {
+                        required : true
+                    })}
                            name="email"
                            type="email"/>
                 </div>
+                {
+                    errors.email && errors.email.type === 'required' ? 
+                    <p style={{ color : 'red',
+                                fontWeight : 'bolder',
+                                margin : '20px'
+                      }}>
+                        Email is required
+                    </p> : null
+                }
                 <div>
                     <label>Password</label>
-                    <input {...register('password')}
+                    <input {...register('password', {
+                        required : true,
+                        minLength : 8
+                    })}
                            name="password"
                            type="password"/>
                 </div>
+                {
+                    errors.password && errors.password.type === 'required' ? 
+                    <p style={{ color : 'red',
+                                fontWeight : 'bolder',
+                                margin : '20px'
+                      }}>
+                        Password is required
+                    </p> : null
+                }
+                {
+                    errors.password && errors.password.type === 'minLength' ?
+                    <p style={{ color : 'red',
+                        fontWeight : 'bolder',
+                        margin : '20px'
+                      }}>
+                        Password should be at least 8 characters long. Please verify the password entered
+                    </p> : null 
+                }
                 <button type="submit">Submit</button>
             </form>
         </div>
